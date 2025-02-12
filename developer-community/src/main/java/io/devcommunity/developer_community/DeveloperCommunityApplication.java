ebcd6ef0825ba18901d329c3aec50305a8f671bb
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-02-12 19:51:28 +0900

    project start

---
+++ developer-community/src/main/java/io/devcommunity/developer_community/DeveloperCommunityApplication.java
@@ -0,0 +1,13 @@
+package io.devcommunity.developer_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperCommunityApplication.class, args);
+	}
+
+}
---
+++ developer-community/src/test/java/io/devcommunity/developer_community/DeveloperCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devcommunity.developer_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
